42454850336252f13fd774b50ab84bbfd2c3fcbe
sysop <sysop@zipato.com>
2016-06-21 14:56:09 +0200

    Added mail reporting to file backup script

--- s3-ftp-backup.sh
+++ s3-ftp-backup.sh
@@ -2,4 +2,19 @@
 
 # Backup from directory on server
 
+# Hostname
+HOST=""
+
+# Admin email
+MAIL=""
+
+# Email Tag
+EMAIL_SUBJECT_TAG="[backup of $HOST]"
+
 /usr/local/bin/s3cmd sync -v --recursive /home/backups/* s3://s3-bucket-name
+
+if [ "$?" = "0"]; then 
+	echo "Daily backup successful. | mail -s "$EMAIL_SUBJECT_TAG Backup successful" $MAIL
+else
+	echo "Daily backup failed. | mail -s "$EMAIL_SUBJECT_TAG Backup failed" $MAIL
+fi
